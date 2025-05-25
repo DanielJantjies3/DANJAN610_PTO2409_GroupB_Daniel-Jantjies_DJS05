f807f5bc66c86858e9d14950084902106769b31c
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-05-25 13:51:17 +0200

    Store Implementation: Create Store, Get State and Dispatch Updates

---
+++ tallyApp.js
@@ -0,0 +1,16 @@
+// Store Implementation (Create Store Function)
+
+function createStore(reducer) {
+    let state;
+    let listeners = [];
+
+//Get Current State
+const getState = () => state;
+
+//Subscribe Changes and Update State (Dispatch Function)
+const dispatch = (action) => {
+    state = reducer (state, action);
+    listeners.forEach(listener => listener());
+};
+
+}
